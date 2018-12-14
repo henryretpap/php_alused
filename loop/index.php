1d86f00e5f983b1582b58ab5f62e0bc9d110c79a
henry.retpap <henry.retpap@khk.ee>
2018-12-14 14:02:27 +0200

    tsüklid - for arvude

---
+++ loop/index.php
@@ -0,0 +1,14 @@
+<?php
+// for
+/*
+ * for($jmn = alg: $jmn <= lopp; $jmn++){
+ *      tegevused mis toimuvad nii kaua
+ *      kui jmn vääärtus ei ole lõppväärtust
+ *      saavutanud
+ * }
+ * */
+echo '<h5>Ülessanne 1</h5>';
+for($arv = 1; $arv <= 10; $arv++){
+
+    echo '<div>'.$arv.'</div>';
+}
