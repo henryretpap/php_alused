eed0febe2cdce6b1de4ff873699a19b46dfefc7f
henry.retpap <henry.retpap@khk.ee>
2018-12-14 12:32:16 +0200

    matemaatilised süsteemid - matemaatilised- ja võrdlusoperaatorid

--- variables/index.php
+++ variables/index.php
@@ -1,32 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: henry.retpap
- * Date: 13.12.2018
- * Time: 13:08
- */
-// muutujate defineerimine
-// $muutujaNimi = väärtus;
-$taisArv = 3;
-$reaalArv = 3.3;
-$sone = '3';
-$toevaartusTrue = true;
-$toevaartusFalse = false;
-// väljastame muutujate väärtusec koos selgitustega
-echo 'Täisarv'.$taisArv.'<br>';
-echo 'Reaalarv'.$reaalArv.'<br>';
-echo 'Sõne'.$sone.'<br>';
-echo 'True'.$toevaartusTrue.'<br>';
-echo 'False'.$toevaartusFalse.'<br>';
-echo '<hr>';
-//tüübikontroll
-echo gettype($sone);
-echo '<br>';
-$summa = $sone + $sone;
-settype($summa,  'string');
-echo gettype($summa);
-echo 'Summa = '.$umma.'<br>';
+// matemaatilised süsteemid
+$arv1 = 42;
+$arv2 = 11;
 
+echo $arv1. ' + '.$arv2.' = '.($arv1 + $arv2).'<br>';
+echo $arv1. ' - '.$arv2.' = '.($arv1 - $arv2).'<br>';
+echo $arv1. ' * '.$arv2.' = '.($arv1 * $arv2).'<br>';
+echo $arv1. ' / '.$arv2.' = '.($arv1 / $arv2).'<br>';
+echo $arv1. ' % '.$arv2.' = '.($arv1 % $arv2).'<br>';
+
+echo '<hr>';
+
+echo $arv1. ' == '.$arv2.' on '.($arv1 == $arv2).'<br>';
+echo $arv1. ' != '.$arv2.' on '.($arv1 != $arv2).'<br>';
+echo $arv1. ' <= '.$arv2.' on '.($arv1 <= $arv2).'<br>';
+echo $arv1. ' >= '.$arv2.' on '.($arv1 >= $arv2).'<br>';
+echo $arv1. ' < '.$arv2.' on '.($arv1 < $arv2).'<br>';
+echo $arv1. ' > '.$arv2.' on '.($arv1 > $arv2).'<br>';
 
 
 
