796734210fc2d6dc61fe8bac1638c38d09a70389
henry.retpap <henry.retpap@khk.ee>
2018-12-14 12:20:58 +0200

    print - teksti väljastamine

---
+++ print/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henry.retpap
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+print('Tere Maailm!<br>');
+print('PHP alused<br>');
+// echo
+echo 'Nii toimub väljastamine';
+// väärtuste ühendamine .
+echo 'See '.'on '.'kolmest sõnast kokku pandud'.'<br>';
