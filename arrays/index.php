ae30ca228c26de82b2ab0761331d2f905bcaf462
henry.retpap <henry.retpap@khk.ee>
2018-12-18 14:59:32 +0200

    massiivid - loomine ja andmetega täitmine

--- arrays/index.php
+++ arrays/index.php
@@ -1,9 +1,10 @@
 <?php
-$kasutajad = array(
-    'alice',
-    'bob',
-    'lucy'
-);
+$kasutajad = array();
+
+$kasutajad[] = 'alice';
+$kasutajad[] = 'bob';
+
+array_push($kasutajad,'lucy');
 
 foreach($kasutajad as $kasutaja){
     echo $kasutaja.'<br>';
